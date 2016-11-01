d058b301717ce07bd8e8b57d57e0ab6f0f8fde69
Erkan Güzelküçük <guzelkucukerkan@gmail.com>
2016-11-01 22:02:13 +0300

    MG-0001 / First Commit

---
+++ MongoDBTest/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDBTest.Models;
+
+namespace MongoDBTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected static IMongoClient client = new MongoClient("mongodb://prod:159632@ds139277.mlab.com:39277/testerkan");
+        protected static IMongoDatabase database = client.GetDatabase("testerkan");
+        UserList UserList;
+        // GET: Home
+        public ActionResult Index()
+        {
+            var col = database.GetCollection<BsonDocument>("Users");
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse("58164c54dcba0f01c12e6b97"));
+
+            var t = col.Find(filter).FirstOrDefault();
+            if (t!=null)
+            {
+                UserList = new UserList()
+                {
+                    Id = t["_id"],
+                    Name = t["Name"].ToString(),
+                    Surname = t["Surname"].ToString(),
+                    Department = t["Deparment"].ToString()
+                };
+            }
+
+            return View(UserList);
+        }
+    }
+}
